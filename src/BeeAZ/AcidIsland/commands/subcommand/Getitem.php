27018e75201ab224a45d8b736761e1f3e8bed6b0
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2023-07-12 07:24:23 +0700

    Create Getitem.php

---
+++ src/BeeAZ/AcidIsland/commands/subcommand/Getitem.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeeAZ\AcidIsland\commands\subcommand;
+
+use BeeAZ\AcidIsland\AcidIsland;
+use pocketmine\command\CommandSender;
+
+class Getitem {
+
+	public function onCommand(CommandSender $player) {
+		AcidIsland::getInstance()->getItem($player));
+    $player->sendMessage("§aSuccesfully get item");
+	}
+}
